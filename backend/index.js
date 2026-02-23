4803ed4ac1b35993b7e5dd20f7247cedf3ef3702
mamtasharma01111 <mamtasharma60069@gmail.com>
2026-02-23 23:29:26 +0530

    First commit

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Backend running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
